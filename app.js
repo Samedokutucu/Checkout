bb6065d1fe7cf086bd5182cf7756a1d9de9334cb
samed <samedokutucu25@gmail.com>
2023-01-25 19:05:34 +0300

    js add - btn

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-const taxtRate = 0.18;
-const shippingPrice = 15;
+const taxRate = 0.18;
+const shippingPrice = 20;
 const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
-  localStorage.setItem("taxtRate", taxtRate);
+  localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
+
+const productsDiv = document.querySelector(".products");
+
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (
+        confirm(
+          `${
+            event.target.parentElement.parentElement.querySelector("h2")
+              .innerText
+          } will be deleted!!!`
+        )
+      ) {
+        event.target.closest(".product").remove();
+      }
+    }
+  } else if (event.target.className == "fa-solid fa-plus") {
+    event.target.parentElement.querySelector(".quantity").innerText++;
+  } else if (event.target.className == "remove-product") {
+    event.target.closest(".product").remove();
+  }
+});
+
+const calculateProductPrice = (btn) => {
+  const productInfoDiv = btn.parentElement.parentElement;
+};
+
+const calculateCartPrice = () => {};
